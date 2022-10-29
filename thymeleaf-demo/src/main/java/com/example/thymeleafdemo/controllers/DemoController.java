8d413c81c52ab68112d12eaf4102ef4959ebb0de
gagan-gv <chordiagagan@gmail.com>
2022-10-29 15:39:27 +0530

    Added Looping example

--- thymeleaf-demo/src/main/java/com/example/thymeleafdemo/controllers/DemoController.java
+++ thymeleaf-demo/src/main/java/com/example/thymeleafdemo/controllers/DemoController.java
@@ -1,16 +1,35 @@
 package com.example.thymeleafdemo.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.thymeleafdemo.models.*;
+
 @Controller
 public class DemoController {
     
+    List<Employee> employees;
+
+    @PostConstruct
+    private void loadData() {
+        employees = new ArrayList<>();
+
+        employees.add(new Employee("Alpha", 1));
+        employees.add(new Employee("Beta", 2));
+        
+    }
+
     @GetMapping("/hello")
     public String sayHello(Model model) {
 
         model.addAttribute("theDate", new java.util.Date());
+        model.addAttribute("employees", employees);
 
         return "helloworld";
     }
