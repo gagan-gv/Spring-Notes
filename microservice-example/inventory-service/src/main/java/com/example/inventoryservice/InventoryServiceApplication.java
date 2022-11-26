26c06fd4c3292db597c71332d1f0fd90e4c6284d
gagan-gv <chordiagagan@gmail.com>
2022-11-26 11:13:43 +0530

    03. Completed Inventory Service

---
+++ microservice-example/inventory-service/src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,35 @@
+package com.example.inventoryservice;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.inventoryservice.models.Inventory;
+import com.example.inventoryservice.repository.InventoryRepository;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+		return args -> {
+			Inventory inventory = new Inventory();
+			inventory.setSkuCode("iPhone_13");
+			inventory.setQuantity(100);
+			
+			inventoryRepository.save(inventory);
+
+			Inventory inventory1 = new Inventory();
+			inventory.setSkuCode("iPhone_13_red");
+			inventory.setQuantity(0);
+
+			inventoryRepository.save(inventory1);
+		};
+	}
+
+}
