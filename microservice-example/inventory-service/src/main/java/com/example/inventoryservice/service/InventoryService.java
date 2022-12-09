39ea55012c763a3a495fcc71dd748d6c4e5d1251
gagan-gv <chordiagagan@gmail.com>
2022-12-09 13:48:04 +0530

    Added Inventory & Order Services

---
+++ microservice-example/inventory-service/src/main/java/com/example/inventoryservice/service/InventoryService.java
@@ -0,0 +1,19 @@
+package com.example.inventoryservice.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.inventoryservice.repository.InventoryRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryService {
+    private final InventoryRepository inventoryRepository;
+
+    @Transactional(readOnly = true)
+    public boolean isInStock(String skuCode) {
+        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    }
+}
