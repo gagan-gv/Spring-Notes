10f5470c572fc23ba04ff0a91606a4c89baddd22
gagan-gv <chordiagagan@gmail.com>
2022-12-11 14:40:13 +0530

    09. Completed Service Discovery

--- microservice-example/inventory-service/src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
+++ microservice-example/inventory-service/src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
@@ -3,12 +3,14 @@ package com.example.inventoryservice;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import com.example.inventoryservice.model.Inventory;
 import com.example.inventoryservice.repository.InventoryRepository;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
--- microservice-example/order-service/src/main/java/com/example/orderservice/service/OrderService.java
+++ microservice-example/order-service/src/main/java/com/example/orderservice/service/OrderService.java
@@ -23,7 +23,7 @@ import lombok.RequiredArgsConstructor;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
     
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -36,7 +36,7 @@ public class OrderService {
         List<String> skuCodes = order.getOrderLineItemsList().stream().map(OrderLineItems::getSkuCode).toList();
 
         // call inventory service and place order if product in stock
-        InventoryResponse[] inventoryResponseArray = webClient.get().uri("http://localhost:8082/api/inventory", uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build()).retrieve().bodyToMono(InventoryResponse[].class).block();
+        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get().uri("http://inventory-service/api/inventory", uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build()).retrieve().bodyToMono(InventoryResponse[].class).block();
 
         boolean allInStock = Arrays.stream(inventoryResponseArray).allMatch(InventoryResponse::isInStock);
 
